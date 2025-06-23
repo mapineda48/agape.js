1987c313d6cf89f0ebc96bb14be2382b4ab32452
Miguel Pineda <a.pinedavegamiguel@gmail.com>
2025-06-23 18:02:48 -0500

    remove devops

--- .azure/core/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "agape_app-rg"
-    storage_account_name = "dedvx6qpkprsolhkkp1um9e1"
-    container_name       = "tfstate"
-    key                  = "core.tfstate"
-  }
-}
--- .azure/deploy/output.tf
+++
@@ -1,4 +0,0 @@
-output "public_ip_address" {
-  value = azurerm_public_ip.public_ip.ip_address
-  description = "Dirección IP pública de la VM para conexión SSH"
-}
