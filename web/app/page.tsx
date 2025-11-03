40eca7b98f0cc8c7c22a38cdc3b3647c56cf1bb3
Miguel Ángel Pineda Vega <mapineda48@personal.wls2>
2025-11-03 11:32:48 -0500

    make  it better

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -20,7 +20,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + VM</h1>
+      <h1>Vite + React + Azure + VM</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
