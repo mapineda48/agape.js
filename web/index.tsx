8a878bf6ad193be25b7997b47c7fcd134477c299
Miguel Ángel Pineda Vega <mapineda48@personal.wls2>
2025-11-29 14:36:31 -0500

    refactor: update error utility import path.

--- web/index.tsx
+++ web/index.tsx
@@ -4,7 +4,7 @@ import EventEmitter from "@/components/util/event-emitter";
 import PortalProvider from "@/components/util/portal.tsx";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import App from "@/app";
-import "@/app/error";
+import "@/utils/error";
 import "./index.css";
 
 /**
@@ -24,26 +24,3 @@ createRoot(document.getElementById("root")!).render(
     </EventEmitter>
   </Enviroment>
 );
-
-// import { Fragment, StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "./assets/index.css";
-// import App from "./app/Router";
-// import EventEmitter from "./components/event-emiter";
-// import "./errors";
-// import PortalProvider from "./components/Portals";
-
-// /**
-//  * https://github.com/facebook/react/issues/24502
-//  */
-// const AppMode = process.env.NODE_ENV === "development" ? Fragment : StrictMode;
-
-// createRoot(document.getElementById("root")!).render(
-//   <AppMode>
-//     <EventEmitter>
-//       <PortalProvider>
-//         <App />
-//       </PortalProvider>
-//     </EventEmitter>
-//   </AppMode>
-// );
